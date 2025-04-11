1d04e62b3729bf977d7e79786459cd0d351608a2
Ashish <ashishabak4@gmail.com>
2025-04-11 13:35:46 +0530

    github-pages setup

--- nashik-police-fe/next.config.ts
+++ nashik-police-fe/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  
+    output: 'export',
+    basePath: isProd ? '/police-website-fe' : '',
+    assetPrefix: isProd ? '/police-website-fe/' : '',
 };
 
 export default nextConfig;
